aa5eea5e86c76f7e5e3243278d3dd53515635cf2
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-08 16:04:42 -0500

    Update MerchantController to access MerchantFacade

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,7 +1,5 @@
 class MerchantsController < ApplicationController 
   def index 
-    @merchants = json[:results].map do |merchant_data| 
-      Merchant.new(merchant_data)
-    end
+    @merchants = MerchantFacade.merchants 
   end
 end
