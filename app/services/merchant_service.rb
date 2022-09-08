2daa558f682b69ffa3270a6b9c1d40b8e5dcf127
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-08 15:54:53 -0500

    Add feature to return Merchants in serialized json

--- app/services/merchant_service.rb
+++ app/services/merchant_service.rb
@@ -1,5 +1,7 @@
 class MerchantService 
   def self.merchants
-    
+    conn = Faraday.new(url: "http://localhost:3000/") 
+    response = conn.get("api/v1/merchants")
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/services/merchant_service_spec.rb
+++ spec/services/merchant_service_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'MerchantService' do 
   it 'retrieves Merchant data and parses response' do 
-    parsed_json = MerchantService.merchant_data
+    parsed_json = MerchantService.merchants
 
     expect(parsed_json).to be_a Hash 
-    expect(parsed_json[:results]).to be_an Array 
+    expect(parsed_json[:data]).to be_an Array 
 
-    merchant_1 = parsed_json[:results][0]
+    merchant_1 = parsed_json[:data][0]
 
-    expect(merchant_1.name).to be_a String 
+    expect(merchant_1[:attributes][:name]).to be_a String 
   end
 end
