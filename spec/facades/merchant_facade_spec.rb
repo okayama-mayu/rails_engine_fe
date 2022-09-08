e3e6530302789449d73699f4747176a00603bb9f
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-08 15:58:53 -0500

    Add test of Merchant Facade

---
+++ spec/facades/merchant_facade_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper' 
+
+RSpec.describe 'MerchantFacade' do 
+  it 'returns an array of Merchant objects' do 
+    merchants = MerchantFacade.merchants 
+
+    expect(merchants).to be_an Array 
+    expect(merchants).to be_all Merchant 
+  end
+end
