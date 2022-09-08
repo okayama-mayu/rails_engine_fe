9cfd4695d56bd6b552c541a27e7bdf44f37d88b5
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-08 16:20:27 -0500

    Add Merchant @id to Merchant poro

--- spec/poros/merchant_spec.rb
+++ spec/poros/merchant_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Merchant do 
   it 'exists and has Merchant attributes' do 
-    data = {name: 'Carlos Stich'}
+    data = {id: 1, name: 'Carlos Stich'}
 
     merchant = Merchant.new(data)
 
     expect(merchant).to be_a Merchant 
+    expect(merchant.id).to eq(data[:id])
     expect(merchant.name).to eq(data[:name])
   end
 end
